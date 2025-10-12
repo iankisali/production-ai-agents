8d098c1a9ba765c34597937e3dd36c3c6f75f1dd
Ian Kisali <iankisali@gmail.com>
2025-10-12 13:14:43 +0300

    updated digital twin to be deployable to aws cloud

--- week2/twin/frontend/components/twin.tsx
+++ week2/twin/frontend/components/twin.tsx
@@ -40,7 +40,7 @@ export default function Twin() {
         setIsLoading(true);
 
         try {
-            const response = await fetch('http://localhost:8000/chat', {
+            const response = await fetch('https://lp693vu6g5.execute-api.us-east-1.amazonaws.com/chat', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
